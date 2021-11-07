e5c5eee91b8596f8c084d056290181b387a030f0
Godsfavour Williams <80661256+GodsfavourWiliiams@users.noreply.github.com>
2021-11-07 17:10:46 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: {
+                "WhiteMode": "url('todo-app-main/images/bg-desktop-light.jpg')"
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
