d74d8bf72a93079d5c2955d56ab9569a54115625
GodzFeivur <godzfeivvur@gmail.com>
2021-11-09 11:16:27 +0100

    feat

--- build/script.js
+++ build/script.js
@@ -1,8 +1,24 @@
 const addtodo = document.getElementById("addtodo");
-const added = document.getElementById("addedTodo");
+const divList = document.querySelector('.listHolder');
 
 function addTodo() {
-    console.log(addtodo.value)
     const userEnteredValue = addtodo.value
-
+    if (userEnteredValue == "") {
+        console.log("the time is 10 pm")
+        addtodo.style.borderColor = "red"
+    } else {
+        addtodo.style.borderColor = "green"
+        const ul = divList.querySelector('ul');
+        const li = document.createElement('li');
+        li.innerHTML = addtodo.value;
+        addtodo.value = '';
+        ul.appendChild(li);
+        createBtn(li);
+    }
 }
+
+addtodo.addEventListener('keyup', (event) => {
+    if (event.which === 13) {
+        addTodo();
+    }
+});
