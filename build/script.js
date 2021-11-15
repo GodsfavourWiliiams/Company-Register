73bae0040e575abe0c7b089c37d85e67db693006
GodzFeivur <godzfeivvur@gmail.com>
2021-11-15 09:29:12 +0100

    feat

--- build/script.js
+++ build/script.js
@@ -30,43 +30,27 @@ function showItem() {
     let itemShow = document.querySelector('.todoLists');
     taskList.forEach((data, index) => {
 
-        // <img src="/todo-app-main/images/list-ul-solid.svg" class="more-button w-6" alt="" onclick="more()">
         add += `
-        <div class="list">
-          <div class="rounded-full w-6 h-6 mt-4 ml-4 border-2 absolute bg-gradient-to-r from-blue-400 to-pink-500 border-white cursor-pointer" onclick="completed()"></div>
-            <div class="py-4 pl-14 px-4 flex justify-between border-b-2 cursor-pointer">
-                 <p class="todo-text text-justify">${data}</p>
-                 <div class="list-container relative flex gap-6">
-            
-                <button type="button" onclick="edittask(${index})" class=""><i class="fa fa-edit"></i>Edit</button>
-                <button type="button" onclick="deleteItem(${index})" class=""><i class="fa fa-trash"></i>Delete</button>
-
-                <style>
-                    .active {
-                        opacity: 1;
-                    }
-                    .completed {
-                        text-decoration: line-through;
-                    }
-                </style>
-                <ul class="menu-button-list bg-white rounded-md py-3 absolute text-black transition duration-500 ease-in left-10 bottom-6 opacity-0 shadow-2xl">
-                    <li class="menu-button-list-item px-3 flex align-center  hover:bg-gray-200 text-black p-2 cursor-pointer relative ">
-                        <i class="fas fa-edit pr-2"></i>
-                        <span class="">Edit</span>
-                    </li>
-                    <li class=" flex align-center text-black p-2 hover:bg-gray-200 px-3  cursor-pointer relative " onClick="openDeleteModal()">
-                        <i class="fas fa-trash-alt pr-2"></i>
-                        <span class="">Delete</span>
-                    </li>
-                </ul>
+        <div class="list border-b-2">
+                        <div class="absolute rounded-full p-2 h-6 w-6 mt-4 ml-4 border-2 bg-gradient-to-r from-blue-400 to-pink-500 dark:border-white border-gray-300 cursor-pointer"></div>
+            <div class="pl-16 pr-2 py-4 flex justify-between cursor-pointer">
+                 <p class="todo-text text-justify pr-6 capitalize">${data}</p>
+               <div class="flex gap-4">
+                 <button type="button" onclick="edittask(${index})"class=""><i class="fa fa-edit"></i></button>
+                 <button type="button" onclick="openDeleteModal(${index})"class=""><i class="fa fa-trash"></i></button>
             </div>
-           </div>
-         </div>
+          </div>
         </div>
 <Style>
 .deleteModal{
     display:block;
 }
+    .active {
+        opacity: 1;
+    }
+    .completed {
+        text-decoration: line-through;
+    }
 </Style>
 <div class="delete hidden fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -96,7 +80,7 @@ function showItem() {
         </div>
       </div>
       <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" onclick="deleteItem(${index})">
           Delete
         </button>
         <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onclick="closeDeleteModal()">
@@ -161,29 +145,20 @@ new Sortable(dragArea, {
     animation: 500
 });
 
+
+
 function deleteItem(index) {
-    let localItems = JSON.parse(localStorage.getItem('localItem'))
-    taskList.splice(index, 1)
-    localStorage.setItem('localItem', JSON.stringify(taskList));
-    showItem()
-};
+    let webtask = localStorage.getItem("localItem");
+    let taskObj = JSON.parse(webtask);
+    taskObj.splice(index, 1);
+    localStorage.setItem("localItem", JSON.stringify(taskObj));
+    showItem();
+}
 
 function clearTask() {
 
     localStorage.clear()
     showItem()
-};
-
-function more() {
-    document.querySelector('.menu-button-list').classList.toggle('active');
-}
-
-function search() {
-    document.querySelector('.search').classList.add('actived');
-}
-
-function cancel() {;
-    document.querySelector('.search').classList.remove('actived');
 };
 
 function openDeleteModal() {;
@@ -193,7 +168,3 @@ function openDeleteModal() {;
 function closeDeleteModal() {;
     document.querySelector('.delete').classList.remove('deleteModal');
 };
-
-function completed() {
-    document.querySelector('.todo-text').classList.toggle('completed');
-}
